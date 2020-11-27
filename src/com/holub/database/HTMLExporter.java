29defac7157ebd683970dcde320ac1684a805da2
DJLee68 <ted4562000@naver.com>
2020-11-28 01:13:05 +0900

    write in html file

--- src/com/holub/database/HTMLExporter.java
+++ src/com/holub/database/HTMLExporter.java
@@ -3,32 +3,66 @@ package com.holub.database;
 import java.io.*;	
 import java.util.*;
 
-import com.holub.database.Table.Exporter;
 
-public class HTMLExporter implements Exporter {
-
-	@Override
-	public void startTable() throws IOException {
-		// TODO Auto-generated method stub
-
+public class HTMLExporter implements Table.Exporter {
+	private final Writer out;
+	public HTMLExporter(Writer out) {
+		this.out = out;
 	}
+	
+	private 	  int	 width;
+	
+	public void startTable() throws IOException {}
 
-	@Override
-	public void storeMetadata(String tableName, int width, int height, Iterator columnNames) throws IOException {
-		// TODO Auto-generated method stub
+	public void storeMetadata( String tableName,
+							   int width,
+							   int height,
+							   Iterator columnNames ) throws IOException
+	{	
+		this.width = width;
+		out.write(tableName == null ? "<anonymous>" : tableName );
+		out.write("<br>");
+		storeRow( columnNames ); // comma separated list of columns ids
+}
 
+	public void storeRow( Iterator data ) throws IOException
+	{	int i = width;
+		while( data.hasNext() )
+		{	Object datum = data.next();
+
+			// Null columns are represented by an empty field
+			// (two commas in a row). There's nothing to write
+			// if the column data is null.
+			if( datum != null )	
+				out.write( datum.toString() );
+
+			if( --i > 0 )
+				out.write(",\t");
+		}
+		out.write("\n");
 	}
-
-	@Override
-	public void storeRow(Iterator data) throws IOException {
-		// TODO Auto-generated method stub
-
-	}
-
+	
 	@Override
 	public void endTable() throws IOException {
 		// TODO Auto-generated method stub
-
 	}
+	public static class Test
+	{ 	public static void main( String[] args ) throws IOException
+		{	
+			Table people = TableFactory.create( "people",
+						   new String[]{ "First", "Last"		} );
+			people.insert( new String[]{ "Allen",	"Holub" 	} );
+			people.insert( new String[]{ "Ichabod",	"Crane" 	} );
+			people.insert( new String[]{ "Rip",		"VanWinkle" } );
+			people.insert( new String[]{ "Goldie",	"Locks" 	} );
+		    
+			Writer out = new FileWriter( "people.html" );
+			
+			HTMLExporter tableBuilder = new HTMLExporter( out );
+			people.export( tableBuilder );
 
+			out.close();
+			
+		}
+	}
 }
