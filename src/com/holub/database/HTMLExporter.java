8f8680a4e12de37d281388721ca8abf00958d57e
DJLee68 <ted4562000@naver.com>
2020-11-30 14:56:14 +0900

    finsh XMLExporter

--- src/com/holub/database/HTMLExporter.java
+++ src/com/holub/database/HTMLExporter.java
@@ -34,6 +34,7 @@ public class HTMLExporter implements Table.Exporter {
 			// (two commas in a row). There's nothing to write
 			// if the column data is null.
 			if( datum != null )	{
+				System.out.println(datum.toString());
 				out.write( datum.toString() );
 			}
 
@@ -41,6 +42,7 @@ public class HTMLExporter implements Table.Exporter {
 				out.write("&emsp;/&emsp;");
 			}
 		}
+		System.out.println();
 		out.write("<br>");
 	}
 	
--- src/com/holub/database/XMLExporter.java
+++ src/com/holub/database/XMLExporter.java
@@ -2,15 +2,46 @@ package com.holub.database;
 
 import java.io.*;	
 import java.util.*;
-
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.charset.StandardCharsets;
+ 
 
 public class XMLExporter implements Table.Exporter {
-	private final Writer out;
-	public XMLExporter(Writer out) {
-		this.out = out;
+	private 	  int	 width;
+	private Document document;
+	private DocumentBuilder docBuilder;
+	private DocumentBuilderFactory docFactory;
+	private Element table;
+	private Element column;
+	private Element row;
+	private boolean isColumn = true;
+	
+	public XMLExporter() {
+		docFactory = DocumentBuilderFactory.newInstance();
+		
+        try {
+			docBuilder = docFactory.newDocumentBuilder();
+			document = docBuilder.newDocument();
+			
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
-	private 	  int	 width;
 	
 	public void startTable() throws IOException {}
 
@@ -19,33 +50,75 @@ public class XMLExporter implements Table.Exporter {
 							   int height,
 							   Iterator columnNames ) throws IOException
 	{	
-		this.width = width;
-		out.write(tableName == null ? "<anonymous>" : tableName );
-		out.write("<p>");
-		storeRow( columnNames ); // comma separated list of columns ids
+		// root 생성
+		table = document.createElement("table");
+		// root 속성 설정
+		if(tableName == null) table.setAttribute("name", "Anonymous");
+		else table.setAttribute("name", tableName);
+		document.appendChild(table);
+		
+		column = document.createElement("column");
+		row = document.createElement("row");
+		table.appendChild(column);
+		table.appendChild(row);
+		storeRow( columnNames); // comma separated list of columns ids
+		
 }
 
-	public void storeRow( Iterator data ) throws IOException
-	{	int i = width;
-		while( data.hasNext() )
-		{	Object datum = data.next();
-
-			// Null columns are represented by an empty field
-			// (two commas in a row). There's nothing to write
-			// if the column data is null.
-			if( datum != null )	{
-				out.write( datum.toString() );
-			}
-
-			if( --i > 0 ) {
-				out.write("&emsp;/&emsp;");
+	public void storeRow(Iterator data) throws IOException
+	{	
+		int i = width;
+		Element row_element = document.createElement("row_element");
+		int col_cnt = 1;
+		while(data.hasNext()) {
+			Object datum = data.next();
+			if(datum!=null) {
+				if(datum!=null && isColumn) {
+					// column 엘리먼트
+		            Element col_name = document.createElement("column_name");
+		            col_name.appendChild(document.createTextNode(datum.toString()));
+		            column.appendChild(col_name);
+				}
+				else {
+					//row element
+					Element row_value = document.createElement("column"+col_cnt);
+					col_cnt++;
+					row_value.appendChild(document.createTextNode(datum.toString()));
+					row_element.appendChild(row_value);
+				}
 			}
 		}
-		out.write("<br>");
+		if(row_element.hasChildNodes()) row.appendChild(row_element);
+		isColumn = false;
 	}
 	
-	@Override
 	public void endTable() throws IOException {
+		
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		 
+        Transformer transformer = null;
+		try {
+			transformer = transformerFactory.newTransformer();
+		} catch (TransformerConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4"); //정렬 스페이스4칸
+        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes"); //들여쓰기
+        transformer.setOutputProperty(OutputKeys.DOCTYPE_PUBLIC, "yes"); //doc.setXmlStandalone(true); 했을때 붙어서 출력되는부분 개행
+        DOMSource source = new DOMSource(document);
+        StreamResult result = new StreamResult(new FileOutputStream(new File("people.xml")));
+
+        try {
+			transformer.transform(source, result);
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		// TODO Auto-generated method stub
 	}
 	public static class Test
@@ -58,12 +131,9 @@ public class XMLExporter implements Table.Exporter {
 			people.insert( new String[]{ "Rip",		"VanWinkle" } );
 			people.insert( new String[]{ "Goldie",	"Locks" 	} );
 		    
-			Writer out = new FileWriter( "people.xml" );
+			XMLExporter tableBuilder = new XMLExporter();
 			
-			XMLExporter tableBuilder = new XMLExporter( out );
-			people.export( tableBuilder );
-
-			out.close();
+            people.export( tableBuilder );
 			
 		}
 	}
